804d950c3bfc32122a92b1225d5614440efeb554
Prateek <prateek.kumar.ind@gmail.com>
2025-06-23 14:10:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from "../Components/Counter";
+import Header from "../Components/Header";
+import Main from "../Components/Main";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      {/* <Counter/> */}
+    </>
+  );
+};
+
+export default App;
