4078f9830a3f4c2e1e2e9050b77eb634333d1cff
Prateek <prateek.kumar.ind@gmail.com>
2025-06-30 13:44:56 +0530

    bug fix

--- ai.js
+++ ai.js
@@ -1,4 +1,4 @@
-import { HfInference, InferenceClient } from "@huggingface/inference";
+import { inference, InferenceClient } from "@huggingface/inference";
 
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page.
@@ -14,7 +14,7 @@ export async function getRecipeFromMistral(ingredientsArr) {
 
   I have: ${ingredientsString}.
   Please give me a recipe you'd recommend I make.
-  `; 
+  `;
 
   try {
     const response = await hf.textGeneration({
